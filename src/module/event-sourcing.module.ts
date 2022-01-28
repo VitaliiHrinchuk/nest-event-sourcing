0635b10980ff65186b0cd56cf37e8d469c49ee6b
Vitalii Hrinchuk <locopiowp@gmail.com>
2022-01-28 20:59:05 +0200

    feat: forRootAsync method

--- src/module/event-sourcing.module.ts
+++ src/module/event-sourcing.module.ts
@@ -1,22 +1,21 @@
-import {DynamicModule, Module} from "@nestjs/common";
-import {EventSourcingOptions} from "./interfaces";
+import {DynamicModule, Module, Provider} from "@nestjs/common";
+import {EventSourcingAsyncOptions, EventSourcingOptions, EventSourcingOptionsFactory} from "./interfaces";
 import {EventDispatcher} from "../dispatcher";
 import {EventStore} from "../event-store";
 import {EventSourcedRepository} from "../repository";
 import {HandlersMapNode} from "./interfaces";
+import {EVENT_DISPATCHER_HANDLERS, EVENT_SOURCING_OPTIONS} from "./event-sourcing.constants";
 
 @Module({})
 export class EventSourcingModule {
     static forRoot(options: EventSourcingOptions): DynamicModule {
-        const dispatcher = new EventDispatcher();
         const eventStore: EventStore = options.driver;
-        const repository = new EventSourcedRepository(eventStore, dispatcher);
         return {
             module: EventSourcingModule,
             providers: [
                 {
                     provide: EventDispatcher,
-                    useValue: dispatcher,
+                    useValue: new EventDispatcher(),
                 },
                 {
                     provide: 'EventStore',
@@ -27,7 +26,10 @@ export class EventSourcingModule {
                 },
                 {
                     provide: EventSourcedRepository,
-                    useValue: repository
+                    useFactory: (dispatcher: EventDispatcher, eventStore: EventStore) => {
+                        return new EventSourcedRepository(eventStore, dispatcher)
+                    },
+                    inject: [EventDispatcher, 'EventStore']
                 }
             ],
             exports: [EventSourcedRepository, EventDispatcher],
@@ -35,13 +37,75 @@ export class EventSourcingModule {
         };
     }
 
+    static forRootAsync(options: EventSourcingAsyncOptions): DynamicModule {
+
+        return {
+            module: EventSourcingModule,
+            imports: options.imports || [],
+            providers: [
+                ...this.createAsyncProviders(options),
+                {
+                    provide: EventDispatcher,
+                    useValue: new EventDispatcher(),
+                },
+                {
+                    provide: 'EventStore',
+                    useFactory: async (options: EventSourcingOptions) => {
+                        const eventStore: EventStore = options.driver;
+                        await eventStore.init();
+                        return eventStore;
+                    },
+                    inject: [EVENT_SOURCING_OPTIONS]
+                },
+                {
+                    provide: EventSourcedRepository,
+                    useFactory: (dispatcher: EventDispatcher, eventStore: EventStore) => {
+                        return new EventSourcedRepository(eventStore, dispatcher)
+                    },
+                    inject: [EventDispatcher, 'EventStore']
+                }
+            ],
+            exports: [EventSourcedRepository, EventDispatcher],
+            global: true
+        };
+    }
+
+    private static createAsyncProviders(options: EventSourcingAsyncOptions): Provider[] {
+        if (options.useExisting || options.useFactory) {
+            return [this.createAsyncOptionsProvider(options)];
+        }
+        return [
+            this.createAsyncOptionsProvider(options),
+            {
+                provide: options.useClass,
+                useClass: options.useClass
+            }
+        ];
+    }
+
+    private static createAsyncOptionsProvider(options: EventSourcingAsyncOptions): Provider {
+        if (options.useFactory) {
+            return {
+                provide: EVENT_SOURCING_OPTIONS,
+                useFactory: options.useFactory,
+                inject: options.inject || []
+            };
+        }
+        return {
+            provide: EVENT_SOURCING_OPTIONS,
+            useFactory: async (optionsFactory: EventSourcingOptionsFactory) => await optionsFactory.createOptions(),
+            inject: [options.useExisting || options.useClass]
+        };
+    }
+
+
     static forFeature(handlers: HandlersMapNode[]): DynamicModule {
 
         return {
             module: EventSourcingModule,
             providers: [
                 {
-                    provide: "EVENT_DISPATCHER_HANDLERS",
+                    provide: EVENT_DISPATCHER_HANDLERS,
                     useFactory: (dispatcher: EventDispatcher) => {
                         handlers.forEach(handler => {
                             dispatcher.listen(handler.event.name, handler.handler);
--- src/module/interfaces/event-sourcing-options.interface.ts
+++ src/module/interfaces/event-sourcing-options.interface.ts
@@ -1,5 +1,18 @@
 import {EventStore} from "../../event-store";
+import {ModuleMetadata, Type} from "@nestjs/common";
 
 export interface EventSourcingOptions {
     driver: EventStore
+}
+
+export interface EventSourcingAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
+    useExisting?: Type<EventSourcingOptions>;
+    useClass?: Type<EventSourcingOptions>;
+    useFactory?: (...args: any[]) => Promise<EventSourcingOptions> | EventSourcingOptions;
+    inject?: any[];
+}
+
+
+export interface EventSourcingOptionsFactory {
+    createOptions(): Promise<EventSourcingOptions> | EventSourcingOptions;
 }
